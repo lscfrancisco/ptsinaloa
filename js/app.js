862fd8285f5ecfcf04b7c1ea19db5a6eeb3f3ce7
Francisco Herrera <lscfrancisco@hotmail.com>
2019-10-24 13:58:13 -0600

    modificamos el el app quitando el diagonal

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
 var url = window.location.href;
-var swLocation = "ptsinaloa/sw.js";
+var swLocation = "/ptsinaloa/sw.js";
 
 
 
 if (navigator.serviceWorker){
 
 if(url.includes('127.0.0.1')){
-	swLocation = 'sw.js';
+	swLocation = '/sw.js';
 }
 
 	navigator.serviceWorker.register(swLocation);
